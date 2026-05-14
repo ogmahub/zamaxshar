59589c5418ebc043b6ade494259cd61fa83d33c6
Og'abek Mahmudov <ogabekmahmudov011@gmail.com>
2026-05-14 23:11:14 +0500

    Remove withCredentials to test CORS issue

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "https://zamaxshar.onrender.com/api",
-  withCredentials: true
+  withCredentials: false
 });
 
 export default api;
