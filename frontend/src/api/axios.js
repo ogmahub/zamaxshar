df2f789dc4aa1be4e46ff8ba04bb286f61f31333
Og'abek Mahmudov <guzalesaniva@gmail.com>
2026-05-15 00:26:49 +0500

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "https://zamaxshar.onrender.com/api",
-  withCredentials: false
+  withCredentials: true
 });
 
 export default api;
